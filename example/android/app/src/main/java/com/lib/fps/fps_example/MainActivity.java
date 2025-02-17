8c0d53492f4c800031246c47a6333b80db4e032c
Eoin Conroy <eoinconroy@gmail.com>
2025-02-17 12:14:36 +0000

    Upgraded app to use new gradle file format and new plugin format

--- example/android/app/src/main/java/com/lib/fps/fps_example/MainActivity.java
+++ example/android/app/src/main/java/com/lib/fps/fps_example/MainActivity.java
@@ -1,6 +1,10 @@
 package com.lib.fps.fps_example;
 
 import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugins.firebase.core.FirebaseCorePlugin;
 
 public class MainActivity extends FlutterActivity {
+    // You can keep this empty class or remove it. Plugins on the new embedding
+    // now automatically registers plugins.
 }
